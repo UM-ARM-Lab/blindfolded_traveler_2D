72bd820cc5908d8b85a7bb6307b6ed62ecb0f586
Brad Saund <brad.saund@gmail.com>
2019-05-10 20:01:54 -0400

    Update travis and docker files with name change

--- docker/test.sh
+++ docker/test.sh
@@ -4,4 +4,4 @@
 . /root/catkin_ws/devel_isolated/setup.bash
 
 cd ~/catkin_ws
-catkin_make_isolated --pkg blindfolded_traveler_2D --catkin-make-args run_tests && catkin_test_results
+catkin_make_isolated --pkg blindfolded_traveler_2d --catkin-make-args run_tests && catkin_test_results
